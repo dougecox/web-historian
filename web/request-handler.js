82624d16edbcf07d7d8572bd0ac8fb2a3b5007c3
Hack Reactor Student <student@hackreactor.com>
2017-04-19 21:21:41 -0700

    1 done

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,8 +1,28 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// var indexPage = require('./web/public/index');
+var fs = require('fs');
+exports.paths = {
+  siteAssets: path.join(__dirname, '../web/public'),
+  archivedSites: path.join(__dirname, '../archives/sites'),
+  list: path.join(__dirname, '../archives/sites.txt')
+};
+
+
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+	
+  if (req.method === 'GET') {
+  var indexPage;
+  fs.readFile(exports.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
+		if (err) {
+			throw err;
+		}
+    indexPage = data; 
+		res.end(indexPage);
+	});
+    
+  }
+  
 };
