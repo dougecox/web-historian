52bdf8c39088acd3065379e7ece14b2d5bebb1e1
Hack Reactor Student <student@hackreactor.com>
2017-04-19 20:15:23 -0700

    updated helpers working on request-handlers

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,16 +26,52 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(this.paths.list, 'utf8',  (err, data) => {
+  	if (err) {
+  	  console.log("yo there's an err", err);
+  	  throw err;
+    }
+    // console.log('over here!:', data.split(" "));
+    callback(data.split('\n'));
+  });
+
 };
 
 exports.isUrlInList = function(url, callback) {
+	this.readListOfUrls( function(urls) {
+			  callback( urls.includes(url) );
+			});
+
 };
 
 exports.addUrlToList = function(url, callback) {
+   fs.writeFile(this.paths.list, url, 'utf8', (err) => {
+    	if (err) { 
+    		throw err;
+    	}
+    	else { 
+	    	callback(url);
+        }
+    });
+
+ 
 };
 
 exports.isUrlArchived = function(url, callback) {
+	// find archive list
+    // run isurlisnist on archive
+    fs.stat(this.paths.archivedSites + '/' + url,  (err) => {
+  	(err) ? callback(false) : callback(true);
+    });
 };
 
 exports.downloadUrls = function(urls) {
+	urls.forEach( (url) => {
+		fs.writeFile(this.paths.archivedSites + '/' + url, (err) => {
+			if (err) throw err;
+			console.log('the file has been saved')
+		});
+	});
+
+
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+// var indexPage = require('./web/public/index');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
